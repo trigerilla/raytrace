e7138d1e60db9d19d11d380d09eaffc18bade97a
Pierre Hoffmeister <pierre@hoffmeistermail.de>
2011-10-01 20:14:04 +0200

    listed objects

--- math/ball.java
+++ math/ball.java
@@ -1,13 +1,33 @@
 package math;
 
-public class ball {
+import logic.compObj;
+import java.awt.Color;
+
+public class ball implements compObj{
 	private vec fixpoint;
 	private double rad;
+	private Color col;
+
+	public Color getColor(){
+		return col;
+	}
+
+	public ball(vec fixpoint, double rad, Color c) {
+		super();
+		this.fixpoint = fixpoint;
+		this.rad = rad;
+		this.col = c;
+	}
 	public ball(vec fixpoint, double rad) {
 		super();
 		this.fixpoint = fixpoint;
 		this.rad = rad;
 	}
+
+	public String toString(){
+		return "ball-> fix:"+fixpoint+" rad:"+rad;
+	}
+
 	public double getRad() {
 		return rad;
 	}
@@ -17,5 +37,29 @@ public class ball {
 	public vec getfixpoint() {
 		return fixpoint;
 	}	
+	public double shortestDistanceTo(vec ray){
+		double dist,p,q,r,erg,x1,x2;
+		dist = Double.MAX_VALUE;
+
+		p=func.scaleProd(fixpoint,fixpoint)-(rad*rad);
+		q=(-2)*(func.scaleProd(ray,fixpoint));
+		r=func.scaleProd(ray, ray);
+				
+		erg = ((q*q)-(4*p*r));
+				
+				
+		if(erg>0){
+			x1 = (-q+Math.sqrt(erg))/(2*r);
+			x2 = (-q-Math.sqrt(erg))/(2*r);
+			if(x1< x2){
+				dist = x1*func.dist(new vec(0,0,0), ray);
+			}else{
+				dist = x2*func.dist(new vec(0,0,0), ray);
+			}
+			
+		}
+
+		return dist;
+	}
 	
 }
--- raytrace.java
+++ raytrace.java
@@ -2,6 +2,7 @@ import java.awt.Color;
 
 import visual.*;
 import math.*;
+import logic.*;
 
 public class raytrace {
 	public static void main(String args[]){
@@ -12,95 +13,65 @@ public class raytrace {
 		boolean deb=false;
 		Color tmpCol;
 		double entf;
+
+		//init picture
 		int w=800,h=600;
 		Picture Screen = new Picture(w,h);
 		Screen.setOriginLowerLeft();
 		
+/*
 		ball b1=new ball(new vec(3150,50,0),100);
 		plain dree = new plain(new vec(3000,0,250),new vec(4000,-50,-100), new vec(3000,150,-100));
-		
+*/		
+		//init distance of screen by angle
 		int x = (int) (((double)w)/(2*Math.tan(Math.toRadians((double)30/2))));
 		
-		double p,q,r;
+		//the current ray		
 		vec akt;
 		akt=new vec(0,0,0);
+		akt.setX(x);		
+	
+		//System.out.println("Abstand= "+x);
 		
-		System.out.println("Abstand= "+x);
-		akt.setX(x);
-		
+
+		//fill the list with objects
+		compList rootList;
+		rootList = new compList(new ball(new vec(3150,50,0),100,Color.RED));
+		rootList.addEnd(rootList,new plain(new vec(3000,0,250),new vec(4000,-50,-100), new vec(3000,150,-100),Color.BLUE));
+		//print the list on terminal
+		rootList.printList(rootList);
+		double tmpentf;	
 		for(int y=(-w/2); y<(w/2);y++){
 			for(int z=-(h/2);z<h/2;z++){
+				//set max dist and maxdist-color				
 				entf = Double.MAX_VALUE;
 				tmpCol = Color.BLACK;
-				double tmpentf,x1,x2;
-				vec tmp1,tmp2,tmp3;
 				
-				
+				//set current ray		
 				akt.setY(y);
 				akt.setZ(z);
 				
-				//kugel
-				//vec tmp = new vec(b1.getfixpoint().getX(),b1.getfixpoint().getY(),b1.getfixpoint().getZ());
-				//func.scale(tmp, -1);				
-				p=func.scaleProd(b1.getfixpoint(),b1.getfixpoint())-(b1.getRad()*b1.getRad());
-				q=(-2)*(func.scaleProd(akt,b1.getfixpoint()));
-				r=func.scaleProd(akt, akt);
-				
-				double erg = ((q*q)-(4*p*r));
-				if(deb){
-					System.out.print(" erg= "+erg);
-					System.out.print("p= "+p);
-					System.out.print(" q= "+q);
-					System.out.print(" r= "+r+"; @");
-					System.out.println(y+(w/2)+" ,"+(z+(h/2)));
-				}
-				
-				if(erg>0){
-					x1 = (-q+Math.sqrt(erg))/(2*r);
-					x2 = (-q-Math.sqrt(erg))/(2*r);
-					if(x1< x2){
-						tmpentf = x1*func.dist(new vec(0,0,0), akt);
-					}else{
-						tmpentf = x2*func.dist(new vec(0,0,0), akt);
-					}
+				//check all objects in the list for the shortest distance
+				compList tmpList;
+				tmpList=rootList;
+				while(tmpList.getNext() != null){
+					tmpentf=tmpList.getCur().shortestDistanceTo(akt);
 					if(tmpentf<entf){
-						tmpCol = Color.BLUE;
-						entf = tmpentf;
+						entf=tmpentf;
+						tmpCol = tmpList.getCur().getColor();
 					}
-					
+					tmpList = tmpList.getNext();
+				}
+				tmpentf=tmpList.getCur().shortestDistanceTo(akt);
+				if(tmpentf<entf){
+					entf=tmpentf;
+					tmpCol = tmpList.getCur().getColor();
 				}
 				
-				
-				//dreieck
-				
-				double deta;
-				deta= func.det(akt, dree.getDir1(), dree.getDir2());
-				
-				tmp3 = new vec(dree.getFixpoint().getX(),dree.getFixpoint().getY(),dree.getFixpoint().getZ());
-				func.scale(tmp3, -1);
-				
-				x1=func.det(akt, tmp3, dree.getDir2())/deta;
-				x2=func.det(akt, dree.getDir1(), tmp3)/deta;
-				
-				
-				tmp1 = new vec(dree.getDir1().getX(),dree.getDir1().getY(),dree.getDir1().getZ());
-				tmp2 = new vec(dree.getDir2().getX(),dree.getDir2().getY(),dree.getDir2().getZ());
-				func.scale(tmp1, x1);
-				func.scale(tmp2, x2);
-				
-				if(x1>0 && x2>0 && (x1+x2)<=1){
-					
-					tmpentf = func.dist(new vec(0,0,0), func.add(dree.getFixpoint(),func.add(tmp1, tmp2)));
-					if(tmpentf < entf){
-						tmpCol = Color.CYAN;
-						entf = tmpentf;
-					}
-					
-				}
-				Screen.set(y+(w/2), z+(h/2), tmpCol);
+				//draw in the right color
+				Screen.set(y+(w/2),z+(h/2),tmpCol);
 			}
 		}
-		System.out.println("fix:"+ dree.getFixpoint()+" dir1:"+dree.getDir1()+" dir2:"+dree.getDir2());
 		Screen.show();
 	}
 }
